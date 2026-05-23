ebd863b0e4c2715331ef31f33f3976c567620f9d
juan-gil-05 <gilj06@hotmail.com>
2026-05-23 10:08:45 +0200

    list of tasks view created

--- tasks/views.py
+++ tasks/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
 from .forms import CreateTaskForm
+from .models import Task
 
 
 # Create your views here.
@@ -38,7 +39,8 @@ def signup(request):
 
 
 def tasks(request):
-    return render(request, "tasks.html")
+    tasks = Task.objects.filter(user=request.user)
+    return render(request, "tasks.html", {"tasks": tasks})
 
 
 def create_task(request):
