a5902d62979d422836811eee1235ea42f412ee9d
juan-gil-05 <gilj06@hotmail.com>
2026-05-23 22:14:03 +0200

    task detail view created

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
@@ -41,6 +41,11 @@ def signup(request):
 def tasks(request):
     tasks = Task.objects.filter(user=request.user)
     return render(request, "tasks.html", {"tasks": tasks})
+
+
+def task_detail(request, task_id):
+    task = get_object_or_404(Task, pk=task_id)
+    return render(request, "task_detail.html", {"task": task})
 
 
 def create_task(request):
