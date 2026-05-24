bdee2664f334238daf026f26bd6d72924df4521b
juan-gil-05 <gilj06@hotmail.com>
2026-05-24 17:43:42 +0200

    routes protection

--- tasks/views.py
+++ tasks/views.py
@@ -6,6 +6,7 @@ from django.db import IntegrityError
 from .forms import CreateTaskForm
 from .models import Task
 from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -39,15 +40,21 @@ def signup(request):
         )
 
 
+@login_required
 def tasks(request):
     tasks = Task.objects.filter(user=request.user, datecompleted__isnull=True)
     return render(request, "tasks.html", {"tasks": tasks})
 
+
+@login_required
 def tasks_completed(request):
-    tasks = Task.objects.filter(user=request.user, datecompleted__isnull=False).order_by('-datecompleted')
+    tasks = Task.objects.filter(
+        user=request.user, datecompleted__isnull=False
+    ).order_by("-datecompleted")
     return render(request, "tasks.html", {"tasks": tasks})
 
 
+@login_required
 def task_detail(request, task_id):
     if request.method == "GET":
         task = get_object_or_404(Task, pk=task_id, user=request.user)
@@ -67,6 +74,7 @@ def task_detail(request, task_id):
             )
 
 
+@login_required
 def create_task(request):
     if request.method == "GET":
         return render(request, "create_task.html", {"form": CreateTaskForm})
@@ -85,6 +93,7 @@ def create_task(request):
             )
 
 
+@login_required
 def complete_task(request, task_id):
     task = get_object_or_404(Task, pk=task_id, user=request.user)
     if request.method == "POST":
@@ -92,22 +101,16 @@ def complete_task(request, task_id):
         task.save()
         return redirect("tasks")
 
+
+@login_required
 def delete_task(request, task_id):
     task = get_object_or_404(Task, pk=task_id, user=request.user)
     if request.method == "POST":
         task.delete()
         return redirect("tasks")
-    
-def complete_task(request, task_id):
-    task = get_object_or_404(Task, pk=task_id, user=request.user)
-    print(request.method)
-    if request.method == "POST":
-        print(task)
-        task.datecompleted = timezone.now()
-        task.save()
-        return redirect("tasks")
 
 
+@login_required
 def signout(request):
     logout(request)
     return redirect("home")
