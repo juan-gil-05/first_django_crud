818099b20b0c42460a404ff32caa9519b73e03ce
juan-gil-05 <gilj06@hotmail.com>
2026-05-24 17:34:36 +0200

    List of tasks completed created

--- tasks/views.py
+++ tasks/views.py
@@ -43,6 +43,10 @@ def tasks(request):
     tasks = Task.objects.filter(user=request.user, datecompleted__isnull=True)
     return render(request, "tasks.html", {"tasks": tasks})
 
+def tasks_completed(request):
+    tasks = Task.objects.filter(user=request.user, datecompleted__isnull=False).order_by('-datecompleted')
+    return render(request, "tasks.html", {"tasks": tasks})
+
 
 def task_detail(request, task_id):
     if request.method == "GET":
